849ed1f6c5682f3b0870340855c58ee0acc1fb62
Jeet <jeetnath2110@gmail.com>
2025-01-26 21:04:26 +0530

    j

--- footer.php
+++ footer.php
@@ -1,3 +1,16 @@
-<?php wp_footer();?>
+<section class="stay-tuned px-3">
+    <div class="container-fluid p-5 d-flex justify-content-center align-items-center">
+        <div class="card border-0 rounded w-75" style="background-color: #0d6efd2b;">
+            <div class="card-body">
+                
+            </div>
+        </div>
+    </div>
+</section>
+<footer>
+
+</footer>
+<?php wp_footer(); ?>
 </body>
+
 </html>
