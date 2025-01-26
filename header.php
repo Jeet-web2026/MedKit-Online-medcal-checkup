ec5c463eff05eb4750aefc5320467d1cade3110b
Jeet <jeetnath2110@gmail.com>
2025-01-26 23:27:12 +0530

    j

--- header.php
+++ header.php
@@ -4,7 +4,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php bloginfo('title'); ?> - <?php bloginfo('description'); ?></title>
+    <title>
+        <?php
+        if (is_front_page() || is_home()) {
+            bloginfo('name');
+            echo ' - ';
+            bloginfo('description');
+        } else {
+            wp_title('', true, 'right');
+        }
+        ?>
+    </title>
     <link rel="shortcut icon" href="<?php echo get_theme_file_uri('/images/logo.png') ?>" type="image/x-icon">
     <?php wp_head() ?>
 </head>
