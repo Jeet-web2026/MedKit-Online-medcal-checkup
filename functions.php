060d6c3683cfb12838ab230a04dd551c59436b0d
Jeet <jeetnath2110@gmail.com>
2025-01-26 20:58:15 +0530

    j

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 function Medkit()
 {
-    wp_enqueue_script('Bootstrap-js-cdn', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js', array('jquery'), '1.0', true);
+    wp_enqueue_script('Bootstrap-js-cdn', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js', null, '1.0', true);
     wp_enqueue_style('Google-font-cdn', 'https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap');
     wp_enqueue_style('fontawesome-cdn', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css');
     wp_enqueue_style('Bootstrap-css-cdn', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css');
