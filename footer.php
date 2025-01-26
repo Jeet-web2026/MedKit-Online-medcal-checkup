27a120e2c6a7f791b6327a9f9a2ece513b9b059b
Jeet <jeetnath2110@gmail.com>
2025-01-26 21:33:13 +0530

    j

--- footer.php
+++ footer.php
@@ -1,14 +1,33 @@
 <section class="stay-tuned px-3">
     <div class="container-fluid p-5 d-flex justify-content-center align-items-center">
-        <div class="card border-0 rounded w-75" style="background-color: #0d6efd2b;">
-            <div class="card-body">
-                
+        <div class="card border-0 rounded w-100 p-4" style="background-color: #0d6efd2b;">
+            <div class="card-body justify-content-between d-flex">
+                <h4 class="text-capitalize fw-bold">stay tuned connected with MedKit</h4>
+                <div class="d-flex justify-content-end">
+                    <a href="javascript:void(0)" class="fs-5 me-2 rounded-circle bg-light" style="padding: 5px 14px;"><i class="fa-brands fa-facebook-f text-primary"></i></a>
+                    <a href="javascript:void(0)" class="fs-5 me-2 rounded-circle bg-light" style="padding: 5px 10px;"><i class="fa-brands fa-x-twitter text-primary"></i></a>
+                    <a href="javascript:void(0)" class="fs-5 me-2 rounded-circle bg-light" style="padding: 5px 12px;"><i class="fa-brands fa-instagram text-primary"></i></a>
+                    <a href="javascript:void(0)" class="fs-5 me-2 rounded-circle bg-light" style="padding: 5px 10px;"><i class="fa-brands fa-youtube text-primary"></i></a>
+                    <a href="javascript:void(0)" class="fs-5 rounded-circle bg-light" style="padding: 5px 12px;"><i class="fa-brands fa-linkedin-in text-primary"></i></a>
+                </div>
             </div>
         </div>
     </div>
 </section>
-<footer>
-
+<footer class="footer px-3">
+    <div class="container-fluid px-5">
+        <div class="row">
+            <div class="col-md-3">
+                <h5 class="text-capitalize fs-5">quick links</h5>
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+            <div class="col-md-3"></div>
+            <div class="col-md-3"></div>
+            <div class="col-md-3"></div>
+        </div>
+    </div>
 </footer>
 <?php wp_footer(); ?>
 </body>
